b6e3270da9e8abc71eb8a1a26115ffe8f294cddb
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-21 23:00:59 -0300

    fix: screen title and route list
    Adds Home screen to the routes list, fixes screen title position
    and adds key to memoized array.

--- routes.js
+++ routes.js
@@ -1,17 +1,23 @@
 import React, { useMemo } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import HomeScreen from './screens/HomeScreen';
+
 const { Navigator, Screen } = createStackNavigator();
 const routes = [
   { name: 'Home', component: HomeScreen, title: 'Anime News' },
 ];
 
 const Router = () => {
-  const screens = useMemo(routes => (
-    routes.map(({ component, name, title }) => (
-      <Screen name={name} component={component} options={{ title }}/>)
-    )
-  ), [routes]);
+  const screens = useMemo(() => (
+    routes.map(({ component, name, title }) =>
+      <Screen
+        component={component}
+        key={name}
+        name={name}
+        options={{ title, headerTitleAlign: 'center' }}
+      />
+  )), [routes]);
 
   return (
     <Navigator initialRouteName='Home'>
