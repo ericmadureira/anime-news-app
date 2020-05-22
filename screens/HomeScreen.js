6bebf75241555eca6f9dc6403984ad3e57750b4d
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-22 00:20:24 -0300

    feat: fetch anime list
    Adds data fetch on screen load.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 
+import API from '../services/api';
+
 const HomeScreen = () => {
+  const [recentAnimes, setRecentAnimes] = useState([]);
+
+  const getRecentAnimes = useCallback(async () => {
+    try {
+      const emptyList = Array.isArray(recentAnimes) && !recentAnimes.length;
+      if (emptyList){
+        const list = await API.getRecentAnimes();
+        console.log('XML NO COMPONENT: ', list);
+        setRecentAnimes(list);
+      }
+    } catch (e) {
+      console.log('Error while fetching data.');
+    };
+  }, [recentAnimes, setRecentAnimes]);
+
+  useEffect(() => {
+    getRecentAnimes();
+  }, [getRecentAnimes])
+
   return (
     <View>
       <Text>Home screen</Text>
