0c467c8afdc9cb0d9adeea2447be462d1ea697a8
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-21 22:42:42 -0300

    feat: router in the app
    Imports and configure the router in the App entrypoint file.

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import Router from './routes';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
