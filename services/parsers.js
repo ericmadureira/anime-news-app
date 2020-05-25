da7de0f036dcb87d1b4fbd17ed556050637ee9bf
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-25 17:17:48 -0300

    feat: prop rename and random id on parser
    Renames added date prop and adds random id generator for flatlist.

--- services/parsers.js
+++ services/parsers.js
@@ -1,8 +1,9 @@
 const parseAnimeList = json => {
   return json.map(item => ({
+    addedAt: item.date_added[0],
+    id: String(Date.now()+Math.random()),
     title: item.anime[0]._,
     url: item.anime[0].$.href,
-    dateAdded: item.date_added[0],
   }));
 };
 
