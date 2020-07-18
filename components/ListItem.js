54e911664af24ed0cdbf2ab20609344bba7bdecb
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-07-18 16:36:50 -0300

    feat: basic list item styling
    Adds basic styles to the list item component and a default preview
    image.

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import defaultAnime from '../assets/defaultAnime.png';
 
 const ListItem = ({ anime }) => {
   const { addedAt, title, url } = anime;
+  const addedDate = addedAt.split(' ')[0];
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{addedAt}</Text>
-      <Text>{url}</Text>
+    <View style={styles.itemContainer}>
+      <Image source={defaultAnime} style={styles.imagePreview}/>
+      <View style={styles.itemInfo}>
+        <Text style={styles.itemTitle}>{title}</Text>
+        <Text style={styles.itemDate}>{addedDate}</Text>
+      </View>
     </View>
   );
 };
 
 export default ListItem;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    height: 'auto',
+    width: '30%',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+    width: '100%',
+  },
+  itemDate: {
+    fontSize: 10,
+  },
+  itemInfo: {
+    flexDirection: 'column',
+    marginLeft: 10,
+  },
+  itemTitle: {
+    fontWeight: 'bold',
+  },
+});
